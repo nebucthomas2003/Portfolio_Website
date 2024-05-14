b017fc5f37b98ee20729e6ba37c719f38bfee4fc
Nebu C Thomas <120630914+nebucthomas2003@users.noreply.github.com>
2024-05-14 20:42:42 +0530

    changed the resume pdf

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function downloadResume() {
     // Replace 'path_to_your_resume_file.pdf' with the actual path to your resume file
-    const resumeUrl = './assets/NebuCThomasResume Updated- 14.01.2024.pdf';
+    const resumeUrl = './assets/RESUMEFINAL.pdf';
     const link = document.createElement('a');
     link.href = resumeUrl;
     link.download = 'Nebu_C_Thomas_Resume.pdf'; // Specify the filename for download
