2067c0386964dec36b7236a6092e12acdbd2e100
Nebu C Thomas <120630914+nebucthomas2003@users.noreply.github.com>
2024-05-18 16:28:06 +0530

    Corrected  the code

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 function downloadResume() {
-  
-    const resumeUrl = '.assets\OriginalResume.pdf';
-    const link = document.createElement('a');
-    link.href = resumeUrl;
-    link.download = 'Nebu_C_Thomas_Resume.pdf'; 
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  }
-  
+  const resumeUrl = './assets/OriginalResume.pdf'; 
+  const link = document.createElement('a');
+  link.href = resumeUrl;
+  link.download = 'Nebu_C_Thomas_Resume.pdf'; 
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
