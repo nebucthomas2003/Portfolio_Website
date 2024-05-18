42be04dc384871cceaeb365367ce1723613b3039
Nebu C Thomas <120630914+nebucthomas2003@users.noreply.github.com>
2024-05-18 16:25:31 +0530

    updated the code

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function downloadResume() {
   
-    const resumeUrl = 'assets\OriginalResume.pdf';
+    const resumeUrl = '.assets\OriginalResume.pdf';
     const link = document.createElement('a');
     link.href = resumeUrl;
     link.download = 'Nebu_C_Thomas_Resume.pdf'; 
